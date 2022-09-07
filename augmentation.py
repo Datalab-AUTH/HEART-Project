650d86aca9e182efbb450985aa9b3835b7345039
unknown <geo4diale@gmail.com>
2022-09-07 11:40:07 +0300

    Compute total energy of mains before and after subtraction of appliances series

--- augmentation.py
+++ augmentation.py
@@ -19,6 +19,20 @@ import pickle
 # plt.style.use('ggplot')
 # rcParams['figure.figsize'] = (13, 10)
 
+def compute_kWh(power_in_watts):
+    power = (power_in_watts * 0.000277) / 1000
+    return power
+
+
+def compute_total_energy(mains_before, mains_after):
+    mains_before.columns = mains_before.columns.get_level_values(0)
+    total_energy_before = mains_before['power'].sum()
+    print("Total Energy of mains: {}".format(compute_kWh(total_energy_before)))
+
+    mains_after.columns = mains_after.columns.get_level_values(0)
+    total_energy_after = mains_after['power'].sum()
+    print("Total Energy of reduced mains: {}".format(compute_kWh(total_energy_after)))
+
 
 def align_timeseries(mains_series: np.array, appliance_series: np.array):
     """
@@ -52,11 +66,11 @@ def subtract_from_mains(mains_series: pd.Series, appliances_series: pd.Series):
 
     mains_wo_appliance_df = mains_wo_appliance.to_frame()
     mains_wo_appliance_df['timestamp'] = mains_wo_appliance_df.index
-    #print(mains_wo_appliance_df.head())
+    # print(mains_wo_appliance_df.head())
     return mains_wo_appliance_df
 
 
-def subtract_target_appliances_from_mains(mains_series: pd.Series):
+def subtract_target_appliances_from_mains(mains_series: pd.Series, elec):
     appliances = ['microwave', 'fridge', 'washing machine', 'dish washer', 'kettle']
 
     mains_wo_appliances = mains_series
@@ -70,6 +84,11 @@ def subtract_target_appliances_from_mains(mains_series: pd.Series):
     mains_wo_appliances_df = mains_wo_appliances.to_frame()
     mains_wo_appliances_df['timestamp'] = mains_wo_appliances_df.index
     print(mains_wo_appliances_df.head())
+
+    # Calculate total energy in kWh for mains before and after subtraction
+    mains_series_df = mains_series.to_frame()
+    compute_total_energy(mains_series_df, mains_wo_appliances_df)
+
     return mains_wo_appliances_df
 
 
@@ -104,8 +123,11 @@ if __name__ == "__main__":
         - washing machine: meter5
         - dish washer: meter6
         - kettle: meter10
+        
+        - mains: meter54
     """
     uk_dale = DataSet("/mnt/c/Users/gdialektakis/Desktop/torch-nilm-main/datasources/datasets/ukdale.h5")
+    # uk_dale_reduced = DataSet("/mnt/c/Users/gdialektakis/Desktop/torch-nilm-main/datasources/datasets/ukdale_reduced.h5")
 
     # Mains meter is meter54
     uk_dale_df = pd.read_hdf('/mnt/c/Users/gdialektakis/Desktop/torch-nilm-main/datasources/datasets/ukdale.h5',
@@ -114,28 +136,34 @@ if __name__ == "__main__":
     uk_dale_df = uk_dale_df.head(100)
     # Select electricity data from House 1
     elec = uk_dale.buildings[1].elec
-    print('Submeters')
-    print(elec)
-    #print(elec.mains())
+
+    # elec_reduced = uk_dale_reduced.buildings[1].elec
+
+    # print(elec.mains())
     # Prints mains power times series
     # print(elec.mains().power_series_all_data().head())
+
     mains = elec.mains()
     mains_series = elec.mains().power_series_all_data()
+    print('Total energy before:')
+    print(mains.total_energy())
 
     # Subtract the power consumption of the 5 target appliances from mains
-    #mains_wo_appliances = subtract_target_appliances_from_mains(mains_series)
+    mains_wo_appliances = subtract_target_appliances_from_mains(mains_series, elec)
 
-    fridge_series = extract_appliance_timeseries(elec, 'washing machine')
+    mains_wo_appliances.to_hdf('/mnt/c/Users/gdialektakis/Desktop/torch-nilm-main/datasources/datasets'
+                               '/ukdale_reduced.h5',
+                               key='/building1/elec/meter54', format='table')
+
+    print('completed!')
+
+    #reduced_mains = elec_reduced.mains()
+    #reduced_mains_series = reduced_mains.power_series_all_data()
+    #print(reduced_mains.total_energy())
+
+    fridge_series = extract_appliance_timeseries(elec, 'fridge')
 
     mains_series_aligned, fridge_series_aligned = align_timeseries(mains_series, fridge_series)
-
-    print('Mains')
-    # print(mains_series.head())
-    # print(mains_series.tail())
-
-    print('fridge')
-    # print(fridge_series_aligned.head())
-    # print(fridge_series_aligned.tail())
 
     mains_wo_fridge_df = subtract_from_mains(mains_series, fridge_series_aligned)
 
